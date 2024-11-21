0a318cf005377c64436e8ee3de45e00197c8c5f4
Lauren Lascano <lascano@unc.edu>
2024-11-21 13:35:59 -0500

    Add styling

--- frontend-example/src/App.js
+++ frontend-example/src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Homepage from './Component';
-// import { NextUIProvider } from '@nextui-org/react';
+import { NextUIProvider } from '@nextui-org/react';
 
 function App() {
   return (
+    <NextUIProvider>
       <Homepage />
+    </NextUIProvider>
   );
 }
 
--- frontend-example/src/Component.js
+++ frontend-example/src/Component.js
@@ -1,45 +1,57 @@
 // frontend-ex/frontend-example/src/Homepage.js
 
 import React from 'react';
+import { Card, Link } from '@nextui-org/react'; // Import available components
+import './Component.css'; // Import the CSS file
 
 const Homepage = () => {
     return (
-        <div>
-            <h1>Welcome to My Homepage</h1>
+        <div className="container">
+            <h1 className="heading">Welcome to My Homepage</h1>
             
             {/* About Me Section */}
-            <section>
-                <h2>About Me</h2>
-                <p>Hi! I'm [Your Name], a [Your Profession/Field]. I love [Your Interests].</p>
-            </section>
+            <Card className="card">
+                <h2 className="heading">About Me</h2>
+                <p>
+                    Hi! I'm [Your Name], a [Your Profession/Field]. I love [Your Interests].
+                </p>
+            </Card>
 
             {/* Internship Experiences Section */}
-            <section>
-                <h2>Internship Experiences</h2>
-                <ul>
-                    <li>Internship at Company A - [Description]</li>
-                    <li>Internship at Company B - [Description]</li>
+            <Card className="card">
+                <h2 className="heading">Internship Experiences</h2>
+                <ul className="list">
+                    <li className="list-item">Internship at Company A - [Description]</li>
+                    <li className="list-item">Internship at Company B - [Description]</li>
                 </ul>
-            </section>
+            </Card>
 
             {/* Projects Section */}
-            <section>
-                <h2>Projects</h2>
-                <ul>
-                    <li><a href="[Project Link 1]">Project 1</a> - [Description]</li>
-                    <li><a href="[Project Link 2]">Project 2</a> - [Description]</li>
+            <Card className="card">
+                <h2 className="heading">Projects</h2>
+                <ul className="list">
+                    <li className="list-item">
+                        <Link href="[Project Link 1]" className="link">Project 1</Link> - [Description]
+                    </li>
+                    <li className="list-item">
+                        <Link href="[Project Link 2]" className="link">Project 2</Link> - [Description]
+                    </li>
                 </ul>
-            </section>
+            </Card>
 
             {/* Social Media Links Section */}
-            <section>
-                <h2>Connect with Me</h2>
-                <ul>
-                    <li><a href="[LinkedIn Link]">LinkedIn</a></li>
-                    <li><a href="[GitHub Link]">GitHub</a></li>
-                    <li><a href="[Twitter Link]">Twitter</a></li>
+            <Card className="card">
+                <h2 className="heading">Connect with Me</h2>
+                <ul className="list">
+                    <li className="list-item"><Link href="[LinkedIn Link]" className="link">LinkedIn</Link></li>
+                    <li className="list-item"><Link href="[GitHub Link]" className="link">GitHub</Link></li>
+                    <li className="list-item"><Link href="[Twitter Link]" className="link">Twitter</Link></li>
                 </ul>
-            </section>
+            </Card>
+
+            <p style={{ textAlign: 'center', color: '#888' }}>
+                © {new Date().getFullYear()} [Your Name]. All rights reserved.
+            </p>
         </div>
     );
 };
